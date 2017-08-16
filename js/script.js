37ad594833c063f9ce38cb53e1dc2b7b5d52c9d8
Bryce Tham <brycetham@gmail.com>
2017-08-15 17:03:12 -0700

    Implemented Initial Data Storage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	getGreeting();
+	loadData(getButtons);
 });
 
 function getGreeting() {
@@ -30,4 +31,39 @@ function getGreeting() {
 	}
 	$("#greeting").html("Good " + greeting + ", Bryce.");
 	$("#aside").html(comments[Math.floor(Math.random() * comments.length)]);
+}
+
+function getButtons(json) {
+	var content = "";
+	$.each(json, function(category, values){
+		var buttons = "";
+		$.each(values["items"], function(index, item){
+			if (item["enabled"]) {
+				buttons += "<a href=\"" + item["url"] + "\" class=\"btn btn-outline-dark\"><i class=\"fa " + item["icon"] + "\" aria-hidden=\"true\"></i> " + item["name"] + "</a> ";
+			}
+		});
+		if (buttons != "") {
+			content += "<div class=\"row\"><div class=\"col\"><h6>" + values["tagline"] + "</h6>" + buttons + "</div></div>";
+		}
+	});
+	$("#content").html(content);
+}
+
+function saveData(data) {
+	chrome.storage.sync.set({"data":data}, function() {
+        console.log("Saved data.");
+	});
+}
+
+function loadData(onSuccess) {
+	chrome.storage.sync.get("data", function(data) {
+        if (Object.keys(data).length != 0) {
+			onSuccess(data)
+		} else {
+			console.log("Loading default data...");
+			$.getJSON("data.json", function(json){
+				onSuccess(json);
+			});
+		}
+	});
 }
